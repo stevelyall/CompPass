f809e556f5c0cc4cd0c828549835b9948ab6be4f
Steve <stevenlyall@gmail.com>
2016-02-14 10:25:17 -0800

    map shown with current location and starting location marker

--- app/src/main/java/ca/stevenlyall/comppass/MapsActivity.java
+++ app/src/main/java/ca/stevenlyall/comppass/MapsActivity.java
@@ -1,26 +1,145 @@
 package ca.stevenlyall.comppass;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+	private final String TAG = "MapsActivity";
+	LocationManager locationManager;
+	LocationListener locationListener;
+	String bestProvider;
+	double lattitude;
+	double longitude;
+	private final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 	private GoogleMap mMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maps);
+
+		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		// 1. choose the best location provider
+		Criteria criteria = new Criteria();
+		criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+		criteria.setPowerRequirement(Criteria.POWER_HIGH);
+		criteria.setAltitudeRequired(false);
+		criteria.setBearingRequired(false);
+		criteria.setSpeedRequired(false);
+		criteria.setCostAllowed(false);
+		bestProvider = locationManager.getBestProvider(criteria, true);
+
+		Log.d("Location", "1- Recommended Location provider is " + bestProvider);
+
+		// 4. getLastKnownLocation
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			Log.d(TAG, "permissions not granted");
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		Location LastKnownLocation = locationManager.getLastKnownLocation(bestProvider);
+		UpdateLocation(LastKnownLocation, "Last Known Location");
+		// 2. Define a listener that responds to location updates
+		locationListener = new LocationListener() {
+			@Override
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location provider.
+				Log.d("Location", "2- A new location is found by the location provider ");
+				UpdateLocation(location, "Location Changed");
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+
+			}
+		};
+
 		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// check for permissions
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// should explanation be shown?
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+				// TODO explain need for permission
+				Toast.makeText(this, "need location", Toast.LENGTH_LONG).show();
+			} else {
+				ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_FINE_LOCATION);
+			}
+
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(bestProvider, 0, 0, locationListener);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		switch (requestCode) {
+			case PERMISSIONS_REQUEST_FINE_LOCATION: {
+				if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// granted
+				} else {
+					// was denied
+				}
+				return;
+			}
+		}
+	}
+
+	public void UpdateLocation(Location location, String state) {
+		if (location != null) {
+			lattitude = location.getLatitude();
+			longitude = location.getLongitude();
+			Log.d("Location", "** " + state + " ** - Lattitue = " + lattitude + ", and Longitude = " + longitude);
+		}
 	}
 
 
@@ -36,12 +155,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
 		mMap = googleMap;
+		setMapSettings();
 
-		// Add a marker in Sydney and move the camera
-		LatLng sydney = new LatLng(-34, 151);
+		LatLng startLocation = new LatLng(lattitude, longitude);
+		mMap.addMarker(new MarkerOptions().position(startLocation).title("Start Location"));
+		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 13), 2000, null);
+	}
 
-		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+	private void setMapSettings() {
 		mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		mMap.setBuildingsEnabled(true);
+		if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			// TODO: Consider calling
+			//    ActivityCompat#requestPermissions
+			// here to request the missing permissions, and then overriding
+			//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+			//                                          int[] grantResults)
+			// to handle the case where the user grants the permission. See the documentation
+			// for ActivityCompat#requestPermissions for more details.
+			return;
+		}
+		mMap.setMyLocationEnabled(true);
+		UiSettings settings = mMap.getUiSettings();
+		settings.setMapToolbarEnabled(false);
+		settings.setMyLocationButtonEnabled(false);
+
 	}
 }
